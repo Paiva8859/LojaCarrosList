8f1e2b682e577633c59a9c4e5ea8e0f36594991c
Paiva8859 <gabrielaguilarpaiva@gmail.com>
2023-11-21 16:48:24 -0300

    passando os arquivos para o repositório separado

---
+++ LojaCarrosList/src/Carros.java
@@ -0,0 +1,45 @@
+public class Carros {
+    //ATRIBUTOS
+    String marca, modelo, ano, cor;
+
+    //MÉTODOS
+    public Carros(String marca, String modelo, String ano, String cor) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.cor = cor;
+    }
+
+    //GETTERS AND SETTERS
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+}
